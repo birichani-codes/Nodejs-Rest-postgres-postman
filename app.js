57195ee30af27e68034e5e0cab137371808a18a5
birichani-codes <birichani.solomon@gmail.com>
2024-06-01 05:43:08 +0000

    application setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+
+const app = express();
+
+app.get('/', (req,res) =>{
+res.status(200).json({
+status:'sucess',
+message: 'Wohoo! The App is working',
+
+});
+
+
+});
+
+app.listen(5000,()=>{
+console.log('server app and running');
+
+});
