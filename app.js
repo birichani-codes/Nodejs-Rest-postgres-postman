98fcd067b21f597c19b3864cf96bc45bca7db439
birichani-codes <birichani.solomon@gmail.com>
2024-06-01 06:33:16 +0000

    folder structure

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require ('express');
-
+const authRouter = require ('./routes/authRoute');
 const app = express();
 
 app.get('/', (req,res) =>{
 res.status(200).json({
-status:'sucess',
-message: 'Wohoo! The App is working',
+status:'success',
+message: 'Wohoo! The App REST APIs are working',
 
 });
-
-
 });
+
+//all routes
+app.use('/api/v1/auth',authRouter);
 
 app.listen(5000,()=>{
 console.log('server app and running');
---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+const signup = (req,res,next)=>{
+   res.json({
+   status:'success',
+   message: 'signup route are working.',
+
+   });
+
+
+};
+
+module.exports = { signup};
