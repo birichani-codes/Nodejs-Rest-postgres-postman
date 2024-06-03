fbaab29588eba477522dcdb35c1323ff8410d0da
birichani-codes <birichani.solomon@gmail.com>
2024-06-04 01:49:41 +0300

    project setup env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: `${process.cwd()}/.env` });
 const express = require ('express');
 const authRouter = require ('./routes/authRoute');
 const app = express();
@@ -13,7 +14,14 @@ message: 'Wohoo! The App REST APIs are working',
 //all routes
 app.use('/api/v1/auth',authRouter);
 
-app.listen(5000,()=>{
+app.use ('*',(req,res, next)=>{
+ res.status(404).json({
+ status:'fail',
+ message:"Route not found",});
+
+});
+const PORT = process.env.APP_PORT || 4000;
+app.listen(process.env.APP_PORT,()=>{
 console.log('server app and running');
 
 });
